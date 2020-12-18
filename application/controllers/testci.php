e5fd2afbf4daaf9aeceb9affaa58b827e3f6b79d
john <olatayoefficient@gmail.com>
2020-12-18 18:00:50 +0530

    Update testci.php

--- application/controllers/testci.php
+++ application/controllers/testci.php
@@ -1,8 +1,13 @@
 <?php
 class testci extends CI_Controller
 {
-    public function index()
-    {
-        return true;
-    }
+	public function index()
+	{
+		return true;
+	}
+
+	public funtion testfun_name(){
+		$this->db->get_where('admin', array('id'=>$this->session->userdata('ims_id')));
+		$this->db->get('admin');
+	}
 }
